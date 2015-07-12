7d914282192431864879b3ea76ed6fea28f286fc
CharlieGreenman <charlie@Charlies-MacBook-Pro.local>
2015-07-12 16:46:16 -0400

    add clickable grid

--- app/js/app.js
+++ app/js/app.js
@@ -2,28 +2,39 @@ document.addEventListener("DOMContentLoaded", domLoaded, false);
 
 function domLoaded(){
 
+ var c = document.getElementById("canvasGrid");
+ var ctx = c.getContext("2d");
+ var createGrid = document.getElementById("create_grid");
+ var rowCount = document.getElementById("input-for-rows").value;
+  var columnCount = document.getElementById("input-for-columns").value;
+  var pixSize = document.getElementById("input-for-pixel-size").value;
+
 
 function createGridIllustrator(){
-
-    var c = document.getElementById("canvasGrid");
-    var ctx = c.getContext("2d");
-    //module for creating a grid
-   var pixSize = 20, pixCount = 25;
-      for(var r = 0; r < pixCount; r++) {
-        for(var i = 0; i < pixCount; i++) {
-
-          //random color function
-
-          ctx.strokeStyle = "#3e4649";
-          ctx.strokeRect(r * pixSize, i * pixSize, pixSize, pixSize);
-          }
+//module for creating a grid
+  for(var r = 0; r < columnCount; r++) {
+    for(var i = 0; i < rowCount; i++) {
+      ctx.strokeStyle = "#3e4649";
+      ctx.strokeRect(r * pixSize, i * pixSize, pixSize, pixSize);
       }
   }
+}
 
-  var createGrid = document.getElementById("create_grid");
+createGrid.addEventListener("click", createGridIllustrator, false);
+
+function handleClick(e) {
+  //var pixSize = document.getElementById("input-for-pixel-size").value;
+
+	ctx.fillStyle = "black";
+  ctx.fillRect(Math.floor(e.offsetX / pixSize) * pixSize,
+               Math.floor(e.offsetY / pixSize) * pixSize,
+               pixSize, pixSize);
+	}
+
+c.addEventListener("click", handleClick);
 
 
-  createGrid.addEventListener("click", createGridIllustrator, false);
+
 
 }
 
