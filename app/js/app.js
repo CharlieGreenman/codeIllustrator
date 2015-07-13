364641386cf967aa16406140cd7d807f1dd4769a
CharlieGreenman <charlie@Charlies-MacBook-Pro.local>
2015-07-13 00:19:42 -0400

    changed to oop to accomodate

--- app/js/app.js
+++ app/js/app.js
@@ -2,50 +2,50 @@ document.addEventListener("DOMContentLoaded", domLoaded, false);
 
 function domLoaded(){
 
- var bitIllustrator = {
+var s,
+ c = document.getElementById("canvasGrid"),
+ ctx = c.getContext("2d"),
+ bitIllustrator = {
 
     settings: {
       createGrid: document.getElementById("create_grid"),
       rowCount: document.getElementById("input-for-rows").value,
       columnCount: document.getElementById("input-for-columns").value,
       pixSize: document.getElementById("input-for-pixel-size").value
+
+    },
+
+    init: function() {
+      s = bitIllustrator.settings;
+      this.bindActions();
+    },
+
+    bindActions: function() {
+      s.createGrid.addEventListener("click", bitIllustrator.createGridIllustrator, false);
+      c.addEventListener("click", bitIllustrator.handleClick, false);
+    },
+
+    createGridIllustrator: function() {
+      //module for creating a grid
+      for(var r = 0; r < s.columnCount; r++) {
+        for(var i = 0; i < s.rowCount; i++) {
+          ctx.strokeStyle = "#3e4649";
+          ctx.strokeRect(r * s.pixSize, i * s.pixSize, s.pixSize, s.pixSize);
+        }
+      }
+    },
+
+    handleClick: function(e) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(Math.floor(e.offsetX / s.pixSize) * s.pixSize,
+                   Math.floor(e.offsetY / s.pixSize) * s.pixSize,
+                   s.pixSize, s.pixSize);
     }
+
 
   };
 
- var c = document.getElementById("canvasGrid");
- var ctx = c.getContext("2d");
- var createGrid = document.getElementById("create_grid");
- var rowCount = document.getElementById("input-for-rows").value;
- var columnCount = document.getElementById("input-for-columns").value;
- var pixSize = document.getElementById("input-for-pixel-size").value;
-
-
-function createGridIllustrator(){
-//module for creating a grid
-  for(var r = 0; r < columnCount; r++) {
-    for(var i = 0; i < rowCount; i++) {
-      ctx.strokeStyle = "#3e4649";
-      ctx.strokeRect(r * pixSize, i * pixSize, pixSize, pixSize);
-      }
-  }
-}
-
-createGrid.addEventListener("click", createGridIllustrator, false);
-
-function handleClick(e) {
-  //var pixSize = document.getElementById("input-for-pixel-size").value;
-
-	ctx.fillStyle = "black";
-  ctx.fillRect(Math.floor(e.offsetX / pixSize) * pixSize,
-               Math.floor(e.offsetY / pixSize) * pixSize,
-               pixSize, pixSize);
-	}
-
-c.addEventListener("click", handleClick);
-
-
-
+  bitIllustrator.init();
 
 }
 
--- dist/js/app.js
+++ dist/js/app.js
@@ -4,31 +4,45 @@ document.addEventListener("DOMContentLoaded", domLoaded, false);
 
 function domLoaded() {
 
-  var c = document.getElementById("canvasGrid");
-  var ctx = c.getContext("2d");
-  var createGrid = document.getElementById("create_grid");
-  var rowCount = document.getElementById("input-for-rows").value;
-  var columnCount = document.getElementById("input-for-columns").value;
-  var pixSize = document.getElementById("input-for-pixel-size").value;
+  var s,
+      c = document.getElementById("canvasGrid"),
+      ctx = c.getContext("2d"),
+      bitIllustrator = {
 
-  function createGridIllustrator() {
-    //module for creating a grid
-    for (var r = 0; r < columnCount; r++) {
-      for (var i = 0; i < rowCount; i++) {
-        ctx.strokeStyle = "#3e4649";
-        ctx.strokeRect(r * pixSize, i * pixSize, pixSize, pixSize);
+    settings: {
+      createGrid: document.getElementById("create_grid"),
+      rowCount: document.getElementById("input-for-rows").value,
+      columnCount: document.getElementById("input-for-columns").value,
+      pixSize: document.getElementById("input-for-pixel-size").value
+
+    },
+
+    init: function init() {
+      s = bitIllustrator.settings;
+      this.bindActions();
+    },
+
+    bindActions: function bindActions() {
+      s.createGrid.addEventListener("click", bitIllustrator.createGridIllustrator, false);
+      c.addEventListener("click", bitIllustrator.handleClick, false);
+    },
+
+    createGridIllustrator: function createGridIllustrator() {
+      //module for creating a grid
+      for (var r = 0; r < s.columnCount; r++) {
+        for (var i = 0; i < s.rowCount; i++) {
+          ctx.strokeStyle = "#3e4649";
+          ctx.strokeRect(r * s.pixSize, i * s.pixSize, s.pixSize, s.pixSize);
+        }
       }
+    },
+
+    handleClick: function handleClick(e) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(Math.floor(e.offsetX / s.pixSize) * s.pixSize, Math.floor(e.offsetY / s.pixSize) * s.pixSize, s.pixSize, s.pixSize);
     }
-  }
 
-  createGrid.addEventListener("click", createGridIllustrator, false);
+  };
 
-  function handleClick(e) {
-    //var pixSize = document.getElementById("input-for-pixel-size").value;
-
-    ctx.fillStyle = "black";
-    ctx.fillRect(Math.floor(e.offsetX / pixSize) * pixSize, Math.floor(e.offsetY / pixSize) * pixSize, pixSize, pixSize);
-  }
-
-  c.addEventListener("click", handleClick);
+  bitIllustrator.init();
 }
